ebe92e721b9e8a246a2642a8f566fd8b7424f978
takumifujita0023 <takumi.fujita0023@gmail.com>
2025-11-18 11:50:31 +0900

    Update scroll-area.tsx

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -1,48 +1,49 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area';
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
-import { cn } from '@/lib/utils';
+type ScrollAreaProps = React.HTMLAttributes<HTMLDivElement>;
 
-const ScrollArea = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
-  <ScrollAreaPrimitive.Root
-    ref={ref}
-    className={cn('relative overflow-hidden', className)}
-    {...props}
-  >
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+/**
+ * シンプルなスクロールコンテナ。
+ * もともとの shadcn の ScrollArea と同じ名前で export しているので、
+ * 画面側のコードはそのまま使えます。
+ */
+export function ScrollArea({
+  className,
+  children,
+  ...props
+}: ScrollAreaProps) {
+  return (
+    <div
+      className={cn("relative overflow-auto", className)}
+      {...props}
+    >
       {children}
-    </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
-    <ScrollAreaPrimitive.Corner />
-  </ScrollAreaPrimitive.Root>
-));
-ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
+    </div>
+  );
+}
 
-const ScrollBar = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ className, orientation = 'vertical', ...props }, ref) => (
-  <ScrollAreaPrimitive.ScrollAreaScrollbar
-    ref={ref}
-    orientation={orientation}
-    className={cn(
-      'flex touch-none select-none transition-colors',
-      orientation === 'vertical' &&
-        'h-full w-2.5 border-l border-l-transparent p-[1px]',
-      orientation === 'horizontal' &&
-        'h-2.5 flex-col border-t border-t-transparent p-[1px]',
-      className
-    )}
-    {...props}
-  >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
-  </ScrollAreaPrimitive.ScrollAreaScrollbar>
-));
-ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
+type ScrollBarProps = React.HTMLAttributes<HTMLDivElement> & {
+  orientation?: "vertical" | "horizontal";
+};
 
-export { ScrollArea, ScrollBar };
+/**
+ * API 互換用のダミー ScrollBar。
+ * 画面側で <ScrollBar /> を使っていてもクラッシュしないようにしているだけで、
+ * 実際には何も表示しません。
+ */
+export function ScrollBar({
+  className,
+  orientation = "vertical",
+  ...props
+}: ScrollBarProps) {
+  return (
+    <div
+      className={cn("hidden", className)}
+      data-orientation={orientation}
+      {...props}
+    />
+  );
+}
