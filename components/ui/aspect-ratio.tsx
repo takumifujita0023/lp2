bcdf71d1c325ae8fc66057dcbf4563a597b3e9fc
takumifujita0023 <takumi.fujita0023@gmail.com>
2025-11-18 11:36:22 +0900

    Update aspect-ratio.tsx

--- components/ui/aspect-ratio.tsx
+++ components/ui/aspect-ratio.tsx
@@ -1,7 +1,41 @@
-'use client';
+"use client";
 
-import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
+import * as React from "react";
 
-const AspectRatio = AspectRatioPrimitive.Root;
+type AspectRatioProps = React.HTMLAttributes<HTMLDivElement> & {
+  /**
+   * アスペクト比（横/縦）
+   * 例）16 / 9, 4 / 3 など
+   */
+  ratio?: number;
+};
 
-export { AspectRatio };
+export const AspectRatio: React.FC<AspectRatioProps> = ({
+  ratio = 16 / 9,
+  style,
+  children,
+  ...props
+}) => {
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        paddingBottom: `${100 / ratio}%`,
+        ...style,
+      }}
+      {...props}
+    >
+      <div
+        style={{
+          position: "absolute",
+          inset: 0,
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default AspectRatio;
